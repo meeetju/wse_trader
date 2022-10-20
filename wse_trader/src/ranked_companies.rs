883d169f946bc70e700b27e40fa05d0aa00f8ec5
Mateusz Mueller <meeetju@gmail.com>
2022-10-20 23:40:41 +0200

    getting companies data, altman to be improved

--- wse_trader/src/ranked_companies.rs
+++ wse_trader/src/ranked_companies.rs
@@ -1,4 +1,6 @@
 use crate::company;
+use regex::{Regex, Error};
+use serde::__private::de::Content;
 
 #[derive(Debug)]
 pub struct RankedCompanies {
@@ -13,7 +15,37 @@ impl RankedCompanies {
 
     pub fn read_companies(self, url: &str) {
         let res = reqwest::blocking::get(url).unwrap();
-        let table = table_extract::Table::find_first(&res.text().unwrap()).unwrap();
-        println!("{:#?}", table);
+        let content = res.text().unwrap();
+        // println!("{:#?}", content);
+        let table = table_extract::Table::find_first(&content).unwrap();
+        for row in table.into_iter() {
+            let cells = row.as_slice();
+            
+            if cells.len() != 4 {
+                continue;
+            } 
+                
+            let ticker = get_ticker(&cells[0]).unwrap();
+            let altman = get_altman_rating(&cells[2]).unwrap();
+            println!("{:#?}, {:#?}", ticker, altman);
+            
+        } 
+        
+    }
+}
+
+fn get_ticker(cell: &str) -> Result<&str, &str> {
+    let ticker = Regex::new(r">([A-Z0-9]+)[ (A-Z0-9.\-)]*<").unwrap().find(cell);
+    match ticker {
+        Some(content) => Ok(&cell[content.start()+1..content.start()+4]),
+        None => Err("Ticker not found")
+    }
+}
+
+fn get_altman_rating(cell: &str) -> Result<&str, &str> {
+    let ticker = Regex::new(r">([A-D]+[+-]?)</span").unwrap().find(cell);
+    match ticker {
+        Some(content) => Ok(&cell[content.start()+1..content.start()+4]),
+        None => Err("Altman rating not found")
     }
 }
