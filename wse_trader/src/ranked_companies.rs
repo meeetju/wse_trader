312f0086b66e9adb8aaf40dca07a42ff9af65bbf
Mateusz Mueller <meeetju@gmail.com>
2022-10-25 00:15:29 +0200

    Improve parsing methods

--- wse_trader/src/ranked_companies.rs
+++ wse_trader/src/ranked_companies.rs
@@ -1,5 +1,5 @@
 use crate::company;
-use regex::{Regex, Error};
+use regex::{Regex, Error, CaptureMatches};
 use serde::__private::de::Content;
 
 #[derive(Debug)]
@@ -25,27 +25,41 @@ impl RankedCompanies {
                 continue;
             } 
                 
-            let ticker = get_ticker(&cells[0]).unwrap();
-            let altman = get_altman_rating(&cells[2]).unwrap();
-            println!("{:#?}, {:#?}", ticker, altman);
+            let ticker = get_ticker(&cells[0]);
+            let altman = get_altman_rating(&cells[2]);
+            let f_score = get_piotroski_f_score(&cells[3]);
+            println!("{:#?}, {:#?}, {:#?}", ticker, altman, f_score);
+            // panic!("");
             
         } 
         
     }
 }
 
-fn get_ticker(cell: &str) -> Result<&str, &str> {
-    let ticker = Regex::new(r">([A-Z0-9]+)[ (A-Z0-9.\-)]*<").unwrap().find(cell);
-    match ticker {
-        Some(content) => Ok(&cell[content.start()+1..content.start()+4]),
-        None => Err("Ticker not found")
-    }
+fn get_ticker(html: &str) -> Result<&str, &str> {
+    let re = Regex::new(r">([A-Z0-9]{3})").unwrap();
+    get_regex_from_html(html, re, "Ticker not found")
 }
 
-fn get_altman_rating(cell: &str) -> Result<&str, &str> {
-    let ticker = Regex::new(r">([A-D]+[+-]?)</span").unwrap().find(cell);
-    match ticker {
-        Some(content) => Ok(&cell[content.start()+1..content.start()+4]),
-        None => Err("Altman rating not found")
+fn get_altman_rating(html: &str) -> Result<&str, &str> {
+    let re = Regex::new(r">([A-D]{1,3})(\+|\-|)</span>").unwrap();
+    get_regex_from_html(html, re, "Altman rating not found")
+}
+
+fn get_piotroski_f_score(html: &str) -> Result<&str, &str> {
+    let re = Regex::new(r">([0-9])</span>").unwrap();
+    get_regex_from_html(html, re, "Piotroski F-Score not found")
+}
+
+fn get_regex_from_html<'a>(html: &'a str, re: Regex, message: &'a str) -> Result<&'a str, &'a str> {
+    let captures_collection = re.captures_iter(html).collect::<Vec<regex::Captures<'_>>>();
+    match captures_collection.get(0) {
+        Some(captures) => {
+            match captures.get(1) {
+                Some(content) => Ok(&html[content.start()..content.end()]),
+                None => Err(&message)
+            }
+        },
+        None => Err(&message)
     }
 }
